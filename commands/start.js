0e87cd3517129e6f5e8a21f38946dcd93d3a7f47
Husan Eshonqulov <husan.eshonqulov.00@gmail.com>
2023-01-21 13:26:27 +0500

    add keyboards to start command

--- commands/start.js
+++ commands/start.js
@@ -1,14 +1,17 @@
-const path = require('path');
-
 const { bot } = require('../core/bot');
 const { messages } = require('../lib/messages');
+const { keyboards } = require('../lib/keyboards');
 
 bot.start((ctx) => {
   ctx.replyWithPhoto(
-    { source: path.join(__dirname, '..', 'public', 'assets', 'logo.png') },
+    // { source: path.join(__dirname, '..', 'public', 'assets', 'logo.png') },
+    'AgACAgIAAxkBAAOFY8uTTeuok-v0Tfy3Y6gj1plcJCgAAmPGMRsTDFlKaFF0bgduXIQBAAMCAANzAAMtBA',
     {
       caption: messages['start'],
       parse_mode: 'MarkdownV2',
+      reply_markup: {
+        inline_keyboard: keyboards['start'],
+      },
     }
   );
 });
